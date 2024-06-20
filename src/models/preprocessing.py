3df68d5452a9912bc7b2e1bc57a9929ad683b906
nicolasmormiche <nicolas160796@gmail.com>
2024-06-20 21:42:11 +0200

    refactor : fonction pre_processing

--- src/models/preprocessing.py
+++ src/models/preprocessing.py
@@ -15,7 +15,7 @@ Changement à prendre en compte :
 import os
 import re
 import pandas as pd
-import language_labels
+from language_labels import language_labels
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
@@ -103,23 +103,22 @@ def clean_column_descriptif(row):
     return texte
 
 def nettoyer_et_separer(description):
-    description = description.lower()  # convertir en minuscules
     mots = re.findall(r'\b\w+\b', description)  # extraire les mots
     return mots
 
 def word_occurence_by_prdtypecode(X, y):
-    yy = pd.DataFrame()
-    yy['prdtypecode']= y['prdtypecode']
-    yy['descriptif'] = X['descriptif']
+    df = pd.DataFrame()
+    df['prdtypecode']= y['prdtypecode']
+    df['descriptif'] = X['descriptif']
 
     # Appliquer la fonction de nettoyage à chaque ligne de la colonne 'designation'
-    yy['mots'] = yy['descriptif'].apply(nettoyer_et_separer)
+    df['mots'] = df['descriptif'].apply(nettoyer_et_separer)
 
     # Initialiser un dictionnaire vide pour stocker les compteurs par classe
     compteurs_par_classe = {}
 
     # Parcourir chaque groupe de classe
-    for classe, groupe in yy.groupby('prdtypecode'):
+    for classe, groupe in df.groupby('prdtypecode'):
         # Combiner tous les mots de la classe en une seule liste
         tous_les_mots = [mot for liste_mots in groupe['mots'] for mot in liste_mots]
         # Compter les occurrences de chaque mot
@@ -130,19 +129,19 @@ def word_occurence_by_prdtypecode(X, y):
     return df_result
 
 def pre_processing():
-    print("Fusion des colonnes description et designation :\n")
+    print("Fusion des colonnes description et designation\n")
     X, y = fusion_description_designation()
 
-    print("Analyse des langues dans la colonne descriptif :\n")
+    print("Analyse des langues dans la colonne descriptif\n")
     # df_lang = traitement_lang(X)
 
-    print("Ré-échantillonnage du jeu de donnée :\n")
+    print("Nettoyage de la colonne descriptif\n")
+    X['descriptif'] = X.apply(lambda row: clean_column_descriptif(row['descriptif']), axis=1)
+
+    print("Création du DataFrame (nombre d'occurence des mots en fonction du prdtypecode)\n")
+    df_result = word_occurence_by_prdtypecode(X, y)
+
+    print("Ré-échantillonnage du jeu de donnée\n")
     X_train, X_test, y_train, y_test = re_echantillonage(X,y)
 
-    print("Nettoyage de la colonne descriptif:\n")
-    X['descriptif'] = X.apply(lambda row: clean_column_descriptif(row['descriptif']), axis=1)
-    X.to_csv('data/X_preprocessed.csv')
-
-    print("Création du DataFrame (nombre d'occurence des mots en fonction du prdtypecode) :\n")
-    df_result = word_occurence_by_prdtypecode(X, y)
-    print(df_result)
+    return X_train, X_test, y_train, y_test

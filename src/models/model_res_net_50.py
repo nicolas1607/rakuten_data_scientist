e6bd55de30a9eef2b837b1ab462d3926880de30f
nicolasmormiche <nicolas160796@gmail.com>
2024-07-26 15:09:26 +0200

    fix & refacto : lime interprétabilité

--- src/models/model_res_net_50.py
+++ src/models/model_res_net_50.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow_addons as tfa
 import seaborn as sns
-import numpy as np
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications import ResNet50
@@ -12,6 +11,8 @@ from keras.models import Model
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.callbacks import ReduceLROnPlateau
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
 
 def data_augmentation(train_df, test_df, batch_size, size):
 
@@ -67,6 +68,39 @@ def plot_results(model_history):
 
     plt.savefig("reports/figures/resnet50/plot_accuracy_and_loss.png", bbox_inches='tight')
 
+def get_predictions(test_generator, y_test, model, model_name):
+
+    if os.path.exists("models/"+model_name+"_predictions.pkl"):
+        y_pred = pickle.load(open("models/"+model_name+"_predictions.pkl", "rb"))
+    else:
+        y_pred = model.predict(test_generator)
+        pickle.dump(y_pred, open("models/"+model_name+"_predictions.pkl", "wb"))
+
+    label_encoder = LabelEncoder()
+    label_encoder.fit_transform(y_test)
+
+    # y_test = test_generator.classes
+    y_test = y_test.astype(int)
+    
+    y_pred = y_pred.argmax(axis=1)
+    y_pred = label_encoder.inverse_transform(y_pred)
+
+    print(classification_report(y_test, y_pred))
+    confusion_heatmap(y_test, y_pred, model_name)
+
+    return None
+
+def confusion_heatmap(y_test, y_pred, modele_name):
+    
+    conf_mat = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(15, 15)) 
+    sns.heatmap(conf_mat, cmap = "coolwarm", annot=True, fmt="d")
+   
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Matrice de confusion :'+modele_name)
+    plt.savefig("reports/figures/matrice_de_confusion/matrice_confusion_heatmap_"+modele_name+".png", bbox_inches='tight')
+
 def model_resnet50(X_train, X_test, y_train, y_test, size):
     
     model_name = "resnet50"
@@ -86,9 +120,9 @@ def model_resnet50(X_train, X_test, y_train, y_test, size):
         model = pickle.load(open("models/"+model_name+".pkl", "rb"))
         model_history = pickle.load(open("models/"+model_name+"_history.pkl", "rb"))
     else:
-        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(size, size, 3), pooling='avg', classes=num_classes, classifier_activation='softmax', input_tensor=None)
+        reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=patience, min_lr=0.0001)
 
-        reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=patience, min_lr=0.001)
+        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(size, size, 3), pooling='avg', classes=num_classes, classifier_activation='softmax', input_tensor=None)
 
         x = base_model.output
         x = Dense(128, activation='relu')(x)
@@ -118,6 +152,7 @@ def model_resnet50(X_train, X_test, y_train, y_test, size):
         pickle.dump(model_history.history, open("models/"+model_name+"_history.pkl", "wb"))
 
     plot_results(model_history)
+    get_predictions(test_generator, y_test, model, model_name)
 
     loss, accuracy, f1_score = model.evaluate(test_generator)
     print(f'Loss: {loss}, Accuracy: {accuracy}, F1 Score: {f1_score}')
